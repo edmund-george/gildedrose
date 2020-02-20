16f02d8711fae4a2f4aa0c3f657387afad67b6dd
edmund-george <e.george@gmail.com>
2020-02-20 16:15:47 +0000

    Refactored - Backstage Pass check

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,7 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    && !isBackstagePass(item)) {
                 if (isQualityPositive(item)) {
                     updateQualityDecreaseWithAge(item);
                 }
@@ -18,7 +18,7 @@ class GildedRose {
                 updateQualityIncreaseWithAge(item);
                 if (item.quality < 50) {
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePass(item)) {
                         if (item.sellIn < 11) {
                             updateQualityIncreaseWithAge(item);
                         }
@@ -37,7 +37,7 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!isBackstagePass(item)) {
                         if (isQualityPositive(item)) {
                             updateQualityDecreaseWithAge(item);
                         }
@@ -49,6 +49,10 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
     private boolean isQualityPositive(Item item) {

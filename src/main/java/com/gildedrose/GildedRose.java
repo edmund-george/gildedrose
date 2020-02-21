5afd2084ab791a56422cfebf647968ffa15f749d
edmund-george <e.george@gmail.com>
2020-02-21 11:57:18 +0000

    Refactored - Abstract class implementing the interface. Helper methods no longer need the item. Interface using constants to decide which Class to instantiate.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,14 @@
 package com.gildedrose;
+import static com.gildedrose.MyItem.myItem;
 
 class GildedRose {
+
+    public static final String BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+
+
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,37 +17,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            moo(item).updateQuality();
+             myItem(item).updateQuality();
         }
     }
 
-    private MyItem moo(Item item) {
-        if(isNormalItem(item)){
-           return new NormalItem(item);
-        }
-        else if(isAgedBrie(item)){
-            return new AgedBrie(item);
-        }
-        else if(isBackstagePass(item)){
-            return new BackstagePass(item);
-        }
-        return new Sulfuras();
-    }
 
-    private boolean isNormalItem(Item item){
-        return !isAgedBrie(item) && !isBackstagePass(item) && !isSulfuras(item);
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
 
 }
--- src/main/java/com/gildedrose/MyItem.java
+++ src/main/java/com/gildedrose/MyItem.java
@@ -3,6 +3,21 @@ package com.gildedrose;
 public interface MyItem {
 
 
+    static MyItem myItem(Item item){
+        if(item.name==GildedRose.BRIE){
+            return new AgedBrie(item);
+        }
+        else if(item.name==GildedRose.BACKSTAGE_PASS){
+            return new BackstagePass(item);
+        }
+        else if(item.name==GildedRose.SULFURAS){
+            return new Sulfuras();
+        }
+        return new NormalItem(item);
+
+
+    };
+
     void updateQuality();
 
 }
